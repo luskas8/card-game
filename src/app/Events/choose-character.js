21d7af3f6f72ab7b166ef10e858bb3fc665792ab
luskas8 <lucas.anjos@franq.com.br>
2023-08-22 20:00:59 -0300

    feat(events): return game status after someone choose character with success

--- src/app/Events/choose-character.js
+++ src/app/Events/choose-character.js
@@ -1,6 +1,7 @@
-import { Error } from "../../config/Responses.js";
-import chooseCharacterUseCase from "../UseCases/chooseCharacterUseCase.js";
+import { Error } from "../../config/Responses.js"
+import chooseCharacterUseCase from "../UseCases/chooseCharacterUseCase.js"
 import logger from "../logger.js"
+import gameStatusUpdate from "./game-status-update.js"
 
 /**
  * @param {Socket} socket 
@@ -10,14 +11,15 @@ import logger from "../logger.js"
  * @returns {Promise<boolean>}
  */
 export default async function chooseCharacter(socket, io, data) {
-    const response = await chooseCharacterUseCase(socket.id, data);
+    const response = await chooseCharacterUseCase(socket.id, data)
 
     if (response instanceof Error) {
-        logger.error(response);
-        socket.emit("choose-character-error", { error: response.message });
-        return false;
+        logger.error(response)
+        socket.emit("choose-character-error", { error: response.message })
+        return false
     }
 
-    socket.emit("choose-character-success", { character: response });
-    return true;
+    socket.emit("choose-character-success", [response.message])
+    gameStatusUpdate(io)
+    return true
 }
