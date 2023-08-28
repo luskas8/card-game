447bf4ccb73e5fa2ac35994779a4e7e567ebfd67
luskas8 <lucas.anjos@franq.com.br>
2023-08-28 12:01:51 -0300

    refactor: conference use case

--- src/app/UseCases/conferenceUseCase.js
+++ src/app/UseCases/conferenceUseCase.js
@@ -6,19 +6,19 @@ export default async function conferenceUseCase() {
         return Error.forbidden("Not all players was killer")
     }
 
-    const first = Game.players.sort((a, b) => b.score - a.score)[0]
-    const tieList = Game.players.filter(player => player.score === first.score && player.socketID !== first.socketID)
+    const firstWinner = Game.players.sort((a, b) => b.score - a.score)[0]
+    const tieList = Game.players.filter(player => player.socketID !== firstWinner.socketID && player.score === firstWinner.score)
     if (tieList.length > 0) {
-        tieList.push(first)
-        const predator = tieList.sort((a, b) => b.killerScore - a.killerScore)[0]
-        const killersTieList = tieList.filter(player => player.killerScore === predator.killerScore && player.socketID !== predator.socketID)
+        tieList.push(firstWinner)
+        const killerWinner = tieList.sort((a, b) => b.killerScore - a.killerScore)[0]
+        const killersTieList = tieList.filter(player => player.socketID !== killerWinner.socketID && player.killerScore === killerWinner.killerScore)
         if (killersTieList.length > 0) {
-            killersTieList.push(predator)
+            killersTieList.push(killerWinner)
             return Success.message(killersTieList)
         }
 
-        return Success.message([predator])
+        return Success.message([killerWinner])
     }
 
-    return Success.message(first)
+    return Success.message(firstWinner)
 }
