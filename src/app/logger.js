e296cec7762ee4f3f8a374262fc4f57d7e0b290c
luskas8 <gnomo1049@gmail.com>
2023-09-17 22:02:34 -0300

    feat: remove log in files

--- src/app/logger.js
+++ src/app/logger.js
@@ -4,42 +4,14 @@ import __dirname from "../../config/utils.js";
 
 class Logger {
     constructor() {
-        const transport = pino.transport({
-            targets: [
-                {
-                    target: "pino-socket",
-                    options: {
-                        address: "10.10.10.5",
-                        port: 5000,
-                        mode: "tcp",
-                    },
-                },
-                {
-                    target: "pino/file",
-                    options: {
-                        destination: path.resolve(
-                            __dirname(import.meta.url),
-                            "logs",
-                            "app.log"
-                        ),
-                        level: Object.values(pino.levels.labels),
-                        mkdir: true,
-                    },
-                },
-            ],
-        });
-
-        this._logger = pino(
-            {
-                timestamp: pino.stdTimeFunctions.isoTime,
-                formatters: {
-                    level: (label) => {
-                        return { level: label.toUpperCase() };
-                    },
+        this._logger = pino({
+            timestamp: pino.stdTimeFunctions.isoTime,
+            formatters: {
+                level: (label) => {
+                    return { level: label.toUpperCase() };
                 },
             },
-            process.env.NODE_ENV === "prd" ? transport : null
-        );
+        });
 
         this._logger.warn("Logger initialized");
     }
