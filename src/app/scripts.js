272bfd73145600a14bafc8f899a316914636a499
luskas8 <gnomo1049@gmail.com>
2023-10-02 21:49:55 -0300

    feat: fix startGame btn select & implement event-switch

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -7,7 +7,7 @@ const $enterGameBtn = document.querySelector('button[name="new-connection"]');
 const $chooseActionBtn = document.querySelector(
     'button[name="choose-character"]'
 );
-const $startGame = document.querySelector('button[name="choose-character"]');
+const $startGame = document.querySelector('button[name="start-game"]');
 
 const socket = io();
 
@@ -34,15 +34,44 @@ socket.on("connect", () => {
 
         socket.on("choose-character", (data) => {
             console.log("> Choose Character:", data);
+            if (data.success) {
+                $chooseActionBtn.disabled = true;
+                $startGame.disabled = false;
+            }
         });
 
         socket.on("start-game", (data) => {
-            console.log("> Start Game:", data);
+            if (data.success) {
+                $startGame.disabled = true;
+                alert("Game started!")
+            }
         });
 
         socket.on("game-status-update", (data) => {
             console.log("> Game Status Update:", data);
+            switch (data.action) {
+            case "update-players":
+                game.addPlayer(data.data);
+                break;
+            case "start-game":
+                // TODO: Implement a custom start game event
+                // to avoid create new Round with new random killer
+                game.didGameStart = true;
+                break;
+            case "update-game":
+                game.players = data.data.players;
+                break;
+            default:
+                break;
+            }
+            if (data.players.length >= 1) {
+                $startGame.disabled = false;
+            }
         });
+        
+        socket.on('start-game', (data) => {
+            console.log("> Start Game:", data);
+        })
     });
 
     $enterGameBtn.disabled = false;
@@ -52,6 +81,15 @@ socket.on("connect", () => {
             socket.emit("new-connection", {
                 name: "freirart" + Math.random(),
             }),
+        false
+    );
+
+    $startGame.addEventListener(
+        "click",
+        () => {
+            socket.emit("start-game");
+            $startGame.disabled = true;
+        },
         false
     );
 });
