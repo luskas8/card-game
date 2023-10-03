0a78d751c47576d05a6f8b6d86259c19b1ce0e1b
luskas8 <gnomo1049@gmail.com>
2023-10-02 21:51:14 -0300

    feat: event log

--- src/app/Events/index.js
+++ src/app/Events/index.js
@@ -15,21 +15,26 @@ import startGame from "./start-game.js";
 export default function mainEvent(socket, game) {
     socket.on("new-connection", (data) => {
         newConnection(socket, data, game);
+        console.log(`> "new-connection: ${JSON.stringify(game.summary)}`);
     });
 
     socket.on("disconnect", () => {
         disconnect(socket, game);
+        console.log(`> "disconnect": ${JSON.stringify(game.summary)}`);
     });
 
     socket.on("choose-character", (data) => {
         chooseCharacter(socket, data, game);
+        console.log(`> "choose-character: ${JSON.stringify(game.summary)}`);
     });
 
     socket.on("start-game", () => {
         startGame(socket, game);
+        console.log(`> "start-game: ${JSON.stringify(game.summary)}`);
     });
 
     socket.on("error", (err) => {
         logger.error(`Socket error: ${err}`);
+        console.log(`> "error": ${JSON.stringify(game.summary)}`);
     });
 }
