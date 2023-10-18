2d33cf76ed9ec4c8bf2d1a03a88157763a9fcd0a
freirart <artur.santos@stone.com.br>
2023-10-18 13:20:41 -0300

    fix vite relative imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+            "#root/*": resolve(__dirname),
+        },
+    },
+});
