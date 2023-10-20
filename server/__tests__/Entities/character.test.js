44e3b69eddadaced76007f2d79224d645d5a0362
luskas8 <gnomo1049@gmail.com>
2023-10-19 21:20:26 -0300

    feat: entities test - character

--- server/__tests__/Entities/character.test.js
+++ server/__tests__/Entities/character.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, test } from "vitest";
 import { Characters } from "../../src/Entities/Character.js";
 
 describe("Test character entity", () => {
@@ -9,5 +9,12 @@ describe("Test character entity", () => {
     it("should be able to find a character by name", () => {
         const character = Characters.findByName("Zeca");
         expect(character).toBeTruthy();
+    });
+
+    test("should all characters have name and favorite place", () => {
+        Characters.characters.forEach((character) => {
+            expect(character.name).toBeDefined();
+            expect(character.favoriteAction).toBeDefined();
+        });
     });
 });
